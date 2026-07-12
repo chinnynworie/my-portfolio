cfb64602359fba1add2b30b50406db1dc09486a8
Chinenye Lizzy Nworie <chinnynworie@gmail.com>
2026-07-12 18:41:48 +0100

    Add profile photo to About section with improved sizing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,15 @@ function About() {
   return (
     <section id="about" className="about">
       <div className="container">
-        <h2>About Me</h2>
-        {personalInfo.bio.map((paragraph, index) => (
-          <p key={index} className="about-bio">{paragraph}</p>
-        ))}
+        <div className="about-header">
+          <img src="/profile.jpg" alt={personalInfo.name} className="about-photo" />
+          <h2>About Me</h2>
+        </div>
+        <div className="about-text">
+          {personalInfo.bio.map((paragraph, index) => (
+            <p key={index} className="about-bio">{paragraph}</p>
+          ))}
+        </div>
       </div>
     </section>
   );
